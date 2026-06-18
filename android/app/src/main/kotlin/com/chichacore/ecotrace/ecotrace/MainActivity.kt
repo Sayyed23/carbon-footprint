88c07f61132072dc4f1c8458db8326303253187c
Ismail <ismailsayyed131@gmail.com>
2026-06-18 19:09:19 +0530

    chore: remove repository and cleanup all project files

--- android/app/src/main/kotlin/com/chichacore/ecotrace/ecotrace/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.chichacore.ecotrace.ecotrace
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/l10n/l10n.dart
+++
@@ -1,9 +0,0 @@
-/// EcoTrace localization configuration.
-library;
-
-class L10n {
-  L10n._();
-
-  /// Supported locales for the application.
-  static const supportedLocales = ['en', 'hi', 'mr'];
-}
